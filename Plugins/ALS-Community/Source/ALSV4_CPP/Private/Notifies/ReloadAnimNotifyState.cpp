4ead6970ce029f7c56ccbc94f65960b88acc9b4a
iegad <iegad@vip.qq.com>
2024-02-23 19:36:54 +0800

    032-添加换弹匣 音效

---
+++ Plugins/ALS-Community/Source/ALSV4_CPP/Private/Notifies/ReloadAnimNotifyState.cpp
@@ -0,0 +1,12 @@
+#include "Notifies/ReloadAnimNotifyState.h"
+#include UE_INLINE_GENERATED_CPP_BY_NAME(ReloadAnimNotifyState)
+
+void 
+UReloadAnimNotifyState::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, 
+	const FAnimNotifyEventReference& EventReference) {
+}
+
+void 
+UReloadAnimNotifyState::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, 
+	const FAnimNotifyEventReference& EventReference) {
+}
